6815fb17a0ce679ba08022d4472f026462ded49d
T-L-H <tlhanley0803@gmail.com>
2019-10-16 19:26:33 -0400

    Project movie

--- Week03/MovieList/MovieList/MainWindow.xaml.cs
+++ Week03/MovieList/MovieList/MainWindow.xaml.cs
@@ -20,9 +20,27 @@ namespace MovieList
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Movie> Movies = new List<Movie>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Show_Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Movie movie in Movies)
+            {
+                movie.ShowDetails();
+            }
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string title = TitleInput.Text;
+            int ReleaseYear = Convert.ToInt32(ReleaseYearInput.Text);
+            Movie movie = new Movie(title, ReleaseYear);
+            Movies.Add(movie);
+            MessageBox.Show("Your Movie was added!");
         }
     }
 }
